80d50fc7625958c47250ac6abced785d06c92b04
Joshua Gato <joshuagato37@gmail.com>
2025-12-15 16:46:06 +0000

    Added loading to auth state

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,33 +1,41 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-  token: null,
-  userId: null,
-  error: '',
-  // error: [],
-  // isAuthenticated: false,
-  authRedirectPath: '/'
-}
+    loading: false,
+    token: null,
+    userId: null,
+    error: "",
+    // error: [],
+    // isAuthenticated: false,
+    authRedirectPath: "/",
+};
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case actionTypes.AUTH_SUCCESS:
-      // return { ...state, token: action.token, userId: action.userId, isAuthenticated: true };
-      return { ...state, token: action.token, userId: action.userId };
+    switch (action.type) {
+        case actionTypes.AUTH_START_LOADING:
+            return { ...state, loading: true };
 
-    case actionTypes.AUTH_FAIL:
-      // return { ...state, token: null, userId: null, error: action.error, isAuthenticated: false };
-      return { ...state, token: null, userId: null, error: action.error };
+        case actionTypes.AUTH_STOP_LOADING:
+            return { ...state, loading: false };
 
-    case actionTypes.AUTH_LOGOUT:
-      // return { ...state, token: null, userId: null, isAuthenticated: false };
-      return { ...state, token: null, userId: null };
+        case actionTypes.AUTH_SUCCESS:
+            // return { ...state, token: action.token, userId: action.userId, isAuthenticated: true };
+            return { ...state, token: action.token, userId: action.userId };
 
-    case actionTypes.SET_AUTH_REDIRECT_PATH:
-      return { ...state, authRedirectPath: action.path };
+        case actionTypes.AUTH_FAIL:
+            // return { ...state, token: null, userId: null, error: action.error, isAuthenticated: false };
+            return { ...state, token: null, userId: null, error: action.error };
 
-    default: return state;
-  }
+        case actionTypes.AUTH_LOGOUT:
+            // return { ...state, token: null, userId: null, isAuthenticated: false };
+            return { ...state, token: null, userId: null };
+
+        case actionTypes.SET_AUTH_REDIRECT_PATH:
+            return { ...state, authRedirectPath: action.path };
+
+        default:
+            return state;
+    }
 };
 
 export default reducer;
