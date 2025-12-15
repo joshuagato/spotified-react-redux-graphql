a57d009f44f029a7073c249dd2e09e4212f2bc81
Joshua Gato <joshuagato37@gmail.com>
2025-12-15 16:49:40 +0000

    Passed data from child compoennt to parent

--- src/components/Welcome/Login/Login.js
+++ src/components/Welcome/Login/Login.js
@@ -1,90 +1,118 @@
-import React, { Component } from 'react';
-import './Login.scss';
+import React, { Component } from "react";
+import "./Login.scss";
 
-import { withRouter, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 // import { findDOMNode } from 'react-dom';
 
-import FormControls from '../../FormControls/FormControls';
-import * as actions from '../../../store/actions/index';
+import FormControls from "../../FormControls/FormControls";
+import * as actions from "../../../store/actions/index";
 
 class Login extends Component {
-
     state = {
-      visible: false
-    }
+        visible: false,
+    };
 
-    loginHandler = event => {
-      event.preventDefault();
+    loginHandler = (event) => {
+        event.preventDefault();
 
-      const email = this.refs.eml.value;
-      const password = this.refs.pwd.value;
-      this.props.onAuth(email, password);
-    }
+        const email = this.refs.eml.value;
+        const password = this.refs.pwd.value;
+        this.props.onAuth(email, password);
+    };
 
     switchToReset = () => {
-      // this.props.history.replace('/reset-pw');
-      this.props.history.push('/reset-pw');
+        // this.props.history.replace('/reset-pw');
+        this.props.history.push("/reset-pw");
     };
 
     togglePasswordView = () => {
-      const password = this.refs.pwd;
-      
-      if (this.state.visible) {
-        password.setAttribute("type", "password");
-        this.setState({ visible: false });
-      } else {
-        password.setAttribute("type", "text");
-        this.setState({ visible: true });
-      }
-    }
-   
-    render() {
-      let redirect = null;
-      if (this.props.isAuthenticated) redirect = <Redirect to={this.props.authRedirectPath} />;
-      
-      return (
-        <div className="login from-bottom">
-          { redirect }
-          
-          <h2>Login to your account</h2>
-          <form onSubmit={this.loginHandler}>
-            <p className="error-message">{this.props.error}</p>
+        const password = this.refs.pwd;
 
-            <FormControls ref="eml" id="eml" name="email" title="email" plcHolder="e.g joshuagato37@gmail.com" 
-              type="email" changed={this.changeHandler} />
-            
-            <FormControls ref="pwd" id="pwd" name="password" title="password" plcHolder="Your password" 
-              type="password" changed={this.changeHandler} />
-            
-            <button type="button" onClick={this.togglePasswordView}><FontAwesomeIcon icon={faEye} /></button>
-            
-            <button type="submit">Login</button>
-          </form>
-          <p onClick={this.props.switchForm}>Don't have an account yet? Register here!</p>
-          <br />
-          <p onClick={this.switchToReset} className="passwd-reset">Forgotten your password?? Click here to reset!!!</p>
-        </div>
-      );
+        if (this.state.visible) {
+            password.setAttribute("type", "password");
+            this.setState({ visible: false });
+        } else {
+            password.setAttribute("type", "text");
+            this.setState({ visible: true });
+        }
+    };
+
+    sendDataToParent(dataToSend) {
+        this.props.sendDataToParent(dataToSend);
+    }
+
+    componentDidUpdate() {
+        this.sendDataToParent(this.props.loading);
+    }
+
+    render() {
+        let redirect = null;
+        if (this.props.isAuthenticated)
+            redirect = <Redirect to={this.props.authRedirectPath} />;
+
+        return (
+            <div className="login from-bottom">
+                {redirect}
+
+                <h2>Login to your account</h2>
+                <form onSubmit={this.loginHandler}>
+                    <p className="error-message">{this.props.error}</p>
+
+                    <FormControls
+                        ref="eml"
+                        id="eml"
+                        name="email"
+                        title="email"
+                        plcHolder="e.g joshuagato37@gmail.com"
+                        type="email"
+                        changed={this.changeHandler}
+                    />
+
+                    <FormControls
+                        ref="pwd"
+                        id="pwd"
+                        name="password"
+                        title="password"
+                        plcHolder="Your password"
+                        type="password"
+                        changed={this.changeHandler}
+                    />
+
+                    <button type="button" onClick={this.togglePasswordView}>
+                        <FontAwesomeIcon icon={faEye} />
+                    </button>
+
+                    <button type="submit">Login</button>
+                </form>
+                <p onClick={this.props.switchForm}>
+                    Don't have an account yet? Register here!
+                </p>
+                <br />
+                <p onClick={this.switchToReset} className="passwd-reset">
+                    Forgotten your password?? Click here to reset!!!
+                </p>
+            </div>
+        );
     }
 }
 
-const mapStateToProps = state =>  {
-  return {
-    // errors: state.auth.error,
-    error: state.auth.error,
-    token: state.auth.token,
-    isAuthenticated: state.auth.token !== null,
-    authRedirectPath: state.auth.authRedirectPath
-  };
+const mapStateToProps = (state) => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error,
+        token: state.auth.token,
+        isAuthenticated: state.auth.token !== null,
+        authRedirectPath: state.auth.authRedirectPath,
+    };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
-  };
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
