9ca7c595d18aff40d7f36ecdbcfb9a123e8d3583
Joshua Gato <joshuagato37@gmail.com>
2025-12-15 16:50:53 +0000

    Retrived data from child component and used it for react loading overlay

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,29 +1,47 @@
-import React, { Component } from 'react';
-import './Welcome.scss';
-import Login from './Login/Login';
-import Register from './Register/Register';
+import React, { Component } from "react";
+import "./Welcome.scss";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import LoadingOverlay from "react-loading-overlay";
 
 class Welcome extends Component {
+    state = {
+        registered: true,
+        loading: false,
+    };
 
-  state = {
-    registered: true
-  }
+    formSwitchHandler = () => {
+        this.setState((prevState) => {
+            return { registered: !prevState.registered };
+        });
+    };
 
-  formSwitchHandler = () => {
-    this.setState((prevState) => {
-        return { registered: !prevState.registered };
-    });
-  }
+    handleChildData = (loading) => {
+        console.log({ loading });
 
-  render() {
-    let form = this.state.registered ? <Login switchForm={this.formSwitchHandler} /> : <Register switchForm={this.formSwitchHandler} />
+        this.setState({ loading });
+    };
 
-    return (
-      <div className="welcome">
-        { form }
-      </div>
-    );
-  }
+    render() {
+        let form = this.state.registered ? (
+            <Login
+                sendDataToParent={this.handleChildData}
+                switchForm={this.formSwitchHandler}
+            />
+        ) : (
+            <Register switchForm={this.formSwitchHandler} />
+        );
+
+        return (
+            <LoadingOverlay
+                active={this.state.loading}
+                spinner
+                text="Requesting..."
+            >
+                <div className="welcome">{form}</div>
+            </LoadingOverlay>
+        );
+    }
 }
 
 export default Welcome;
